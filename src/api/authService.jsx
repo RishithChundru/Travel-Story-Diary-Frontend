36b032a7d8203e6c4a19b4ec59d154455f521361
Rishith Sai Chowdary Chundru <rishithrishith1@gmail.com>
2025-07-21 22:27:42 +0530

    updated

--- src/api/authService.jsx
+++ src/api/authService.jsx
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-// const API_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:5000/api';
-
-const API_URL = 'https://travel-story-diary-backend.onrender.com/api'; 
+const API_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 // Register
 const register = async (userData) => {
