ffc021ca8008c9f8ccb5bd0246269bf36bd0b1d3
Rishith Sai Chowdary Chundru <rishithrishith1@gmail.com>
2025-07-22 11:51:24 +0530

    updated index

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { store } from './app/store'; // If using Redux
-import { Provider } from 'react-redux'; // If using Redux
+import { store } from './app/store';
+import { Provider } from 'react-redux'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Provider store={store}> {/* Wrap with Provider */}
+        <Provider store={store}> 
             <App />
         </Provider>
     </React.StrictMode>
